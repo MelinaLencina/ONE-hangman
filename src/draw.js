d039af26094ad9b4a16b2ca77188b4eac37875ab
Melina Lencina <melina00lencina@gmail.com>
2022-09-27 21:57:29 -0300

    :spakles: Added functionality

---
+++ src/draw.js
@@ -0,0 +1,122 @@
+function drawCanvas() {
+  table.lineWidth = 8;
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.fillStyle = "#F3F5FC";
+  table.strokeStyle = "#0A3871";
+  table.fillRect(0, 0, 1200, 800);
+  table.beginPath();
+  table.moveTo(650, 500);
+  table.lineTo(900, 500);
+  table.stroke();
+  table.closePath();
+}
+
+function drawLine() {
+  table.lineWidth = 6;
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.strokeStyle = "#0A3871";
+  table.beginPath();
+  let w = 600 / secretWord.length;
+  for (let i = 0; i < secretWord.length; i++) {
+    table.moveTo(500 + w * i, 640);
+    table.lineTo(550 + w * i, 640);
+  }
+  table.stroke();
+  table.closePath();
+}
+function writeRightLetter(index) {
+  table.font = "bold 52px Inter";
+  table.lineWidth = 6;
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.fillStyle = "#0A3871";
+  let w = 600 / secretWord.length;
+  table.fillText(secretWord[index], 505 + w * index, 620);
+  table.stroke();
+}
+
+function writeWrongLetter(letter, errorsLeft) {
+  table.lineWidth = 6;
+  table.font = "bold 40px Inter";
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.fillStyle = "#0A3871";
+  table.fillText(letter, 535 + 40 * (10 - errorsLeft), 710, 40);
+}
+
+function drawHangman(points) {
+  table.lineWidth = 8;
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.strokeStyle = "#0A3871";
+  if (points === 8) {
+    table.moveTo(700, 500);
+    table.lineTo(700, 100);
+  }
+  if (points === 7) {
+    table.moveTo(850, 100);
+    table.lineTo(700, 100);
+  }
+  if (points === 6) {
+    table.moveTo(850, 100);
+    table.lineTo(850, 171);
+  }
+  if (points === 5) {
+    //head
+    table.moveTo(900, 230);
+    table.arc(850, 230, 50, 0, Math.PI * 2);
+  }
+  if (points === 4) {
+    //body
+    table.moveTo(850, 389);
+    table.lineTo(850, 289);
+  }
+  if (points === 3) {
+    //left leg
+    table.moveTo(850, 389);
+    table.lineTo(800, 450);
+  }
+  if (points === 2) {
+    //right leg
+    table.moveTo(850, 389);
+    table.lineTo(890, 450);
+  }
+  if (points === 1) {
+    //left hand
+    table.moveTo(850, 330);
+    table.lineTo(800, 389);
+  }
+  if (points === 0) {
+    //right hand
+    table.moveTo(850, 330);
+    table.lineTo(890, 389);
+  }
+  table.stroke();
+  table.closePath();
+}
+
+function youLose() {
+  table.font = "bold 42px Inter";
+  table.lineWidth = 6;
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.fillStyle = "red";
+  table.fillText("You lose!", 930, 320);
+}
+
+function youWin() {
+  table.font = "bold 42px Inter";
+  table.lineWidth = 6;
+  table.lineCap = "round";
+  table.lineJoin = "round";
+  table.fillStyle = "green";
+  table.fillText("You win,", 950, 320);
+  table.fillText("Congrats!", 930, 360);
+  setTimeout(refresh, 3000);
+}
+
+function refresh() {
+  location.reload();
+}
---
+++ src/script.js
@@ -0,0 +1,178 @@
+let screen = document.querySelector("canvas");
+let buttonNewGame = (document.getElementById(
+  "btn-new-game"
+).style.display = "none");
+let btnExitHidden = (document.getElementById("btn-exit").style.display =
+  "none");
+let divAddWord = (document.getElementById(
+  "add-word"
+).style.display = "none");
+let btnNewGame = document.getElementById("btn-new-game");
+let btnExit = document.getElementById("btn-exit");
+let btnCancel = document.getElementById("btn-cancel");
+
+let words = [
+  "SQUEEZE",
+  "SUSPEND",
+  "CLOUDY",
+  "PARALLEL",
+  "SCRAPE",
+  "PUPPY",
+  "HORSES",
+  "SEDATE",
+  "GUARDED",
+  "PART",
+  "NAME",
+  "SOLID",
+  "QUEUE",
+  "ALIKE",
+  "HOME",
+  "TALENTED",
+  "INCREASE",
+  "SILKY",
+  "RUDE",
+  "HYPNOTIC",
+  "KITTEN",
+  "RABBIT",
+  "OILY",
+  "DELICIOUS",
+  "SNOW",
+  "STICKY",
+  "VIEW",
+];
+const table = document.getElementById("hangman").getContext("2d");
+let secretWord = "";
+let letters = [];
+let rightWord = "";
+let errors = 8;
+let incorrectLetters = [];
+let numErrors = 8;
+let chosenLetter = [];
+
+
+document.getElementById("start-game").onclick = () => {
+  startGame();
+};
+
+document.getElementById("btn-save").onclick = () => {
+  saveWord();
+};
+
+btnNewGame.addEventListener("click", function () {
+  location.reload();
+});
+
+btnExit.addEventListener("click", function () {
+  location.reload();
+});
+
+btnCancel.addEventListener("click", function () {
+  location.reload();
+});
+
+function pickSecretWord() {
+  let word = words[Math.floor(Math.random() * words.length)];
+  secretWord = word;
+  return word;
+}
+
+function verifyClickedLetter(key) {
+  if (letters.length < 1 || letters.indexOf(key) < 0) {
+    letters.push(key);
+    return false;
+  } else {
+    letters.push(key);
+    return true;
+  }
+}
+
+function addRightLetter(i) {
+  rightWord += secretWord[i].toUpperCase();
+}
+
+function addWrongLetter(letter) {
+  if (secretWord.indexOf(letter) <= 0) {
+    errors -= 1;
+  }
+}
+
+function verifyEndGame(letter) {
+  if (chosenLetter.length < secretWord.length) {
+    incorrectLetters.push(letter);
+
+    if (incorrectLetters.length > numErrors) {
+      youLose();
+    } else if (chosenLetter.length < secretWord.length) {
+      addWrongLetter(letter);
+      writeWrongLetter(letter, errors);
+    }
+  }
+}
+
+function verifyWinner(letter) {
+  chosenLetter.push(letter.toUpperCase());
+  if (chosenLetter.length == secretWord.length) {
+    youWin();
+  }
+}
+
+function verifyLetter(keyCode) {
+  if (typeof keyCode === "number" && keyCode >= 65 && keyCode <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function screenAddWord() {
+  document.getElementById("div-hidden").style.display = "none";
+  document.getElementById("add-word").style.display = "block";
+}
+
+function saveWord() {
+  let newWord = document.getElementById("input-new-word").value;
+
+  if (newWord !== "") {
+    words.push(newWord.toUpperCase());
+    alert("Word added successfully!");
+
+    document.getElementById("add-word").style.display = "none";
+    startGame();
+  } else {
+    alert("No word was found.");
+  }
+}
+
+function startGame() {
+  document.getElementById("div-hidden").style.display = "none";
+
+  drawCanvas();
+  pickSecretWord();
+  drawLine();
+
+  document.getElementById("btn-new-game").style.display = "block";
+  document.getElementById("btn-exit").style.display = "block";
+
+  document.onkeydown = (e) => {
+    let letter = e.key.toUpperCase();
+    if (incorrectLetters.length <= numErrors) {
+      if (!verifyClickedLetter(e.key) && verifyLetter(e.keyCode)) {
+        if (secretWord.includes(letter)) {
+          addRightLetter(secretWord.indexOf(letter));
+          for (let i = 0; i < secretWord.length; i++) {
+            if (secretWord[i] === letter) {
+              writeRightLetter(i);
+              verifyWinner(letter);
+            }
+          }
+        } else {
+          if (!verifyClickedLetter(e.key) && !verifyWinner(letter))
+            return;
+          drawHangman(errors);
+          verifyEndGame(letter);
+        }
+      }
+    } else {
+    }
+  };
+}
